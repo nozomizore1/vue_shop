79ed0331209ddf12164d17f0a52ee4201b5e29c5
nozomizore <ptnorreina@outlook.com>
2020-09-07 14:11:33 +0800

    修改登录一部分功能

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,25 +10,26 @@ export default {
     return {}
   },
   methods: {
-    logout: function () {
-      this.$confirm('是否要退出登录?', '提示', {
+    logout: async function () {
+      const result = await this.$confirm('是否要退出登录?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
-      }).then(() => {
-        // 删除token
-        window.sessionStorage.removeItem('token')
-        // 跳转到登录页面
-        this.$router.push('/login')
-        this.$message({
-          type: 'success',
-          message: '退出成功!'
-        })
-      }).catch(() => {
-        this.$message({
-          type: 'info',
-          message: '退出失败'
-        })
+      }).catch(err => {
+        return err
+      })
+      if (result !== 'confirm') {
+        return this.$message('已取消退出操作')
+      }
+      // 删除token
+      window.sessionStorage.removeItem('token')
+      // 删除index
+      window.sessionStorage.removeItem('selectedPage')
+      // 跳转到登录页面
+      this.$router.push('/login')
+      this.$message({
+        type: 'success',
+        message: '退出成功!'
       })
     }
   }
